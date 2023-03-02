f9d0fe4a159f2d6ab58db4c199d6a275b1125371
wangxiaobo <wang_xiaobo@hzlh.com>
2023-03-02 10:51:57 +0800

    chore(base): add spring boot service

---
+++ src/services/app/src/main/java/com/maple/app/Greeting.java
@@ -0,0 +1,5 @@
+package com.maple.app;
+
+public record Greeting(long id, String content) {
+  
+}
---
+++ src/services/app/src/test/java/com/maple/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maple.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
